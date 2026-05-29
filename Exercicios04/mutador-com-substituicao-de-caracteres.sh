bce471d6452f16157ba89649a94c99c4d3da0af1
Weberttz <johngaldencio18@gmail.com>
2026-05-29 20:57:17 -0300

    feat: adiciona mutador-com-substituicao-de-caracteres.sh

---
+++ Exercicios04/mutador-com-substituicao-de-caracteres.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+[[ $# -ne 1 ]] && echo "Passe um arquivo como argumento" && exit 1
+
+[[ !  -f $1 ]] && echo "O arquivo não existe" && exit 1
+
+caminho="./Arquivos/leet.text"
+
+while read palavra; do
+    echo $palavra >> $caminho
+
+    leet=${palavra//a/@};
+    echo $leet >> $caminho
+
+    leet=${palavra//e/3};
+    echo $leet >> $caminho;
+
+    leet=${palavra//i/1};
+    echo $leet >> $caminho;
+
+    leet=${palavra//o/0};
+    echo $leet >> $caminho;
+
+    leet=${palavra//s/$};
+    echo $leet >> $caminho;
+
+done < "$1"
